4be5ef20cae23287456ca60b329ba95e76ee7bf1
Laimonas Simutis <laimis@gmail.com>
2020-01-09 09:20:47 -0500

    implement stock export functionality (#4)

--- src/core/Options/CSVExport.cs
+++ src/core/Options/CSVExport.cs
@@ -7,21 +7,37 @@ namespace core.Options
 {
     public class CSVExport
     {
+        public static string Generate(IEnumerable<OwnedStock> stocks)
+        {
+            return Generate(
+                GetStocksExportHeaders(),
+                stocks,
+                o => GetExportParts((o as OwnedStock).State));
+        }
+
         public static string Generate(IEnumerable<SoldOption> stocks)
+        {
+            return Generate(
+                GetOptionsExportHeaders(),
+                stocks,
+                o => GetExportParts((o as SoldOption).State));
+        }
+
+        private static string Generate(string header, IEnumerable<object> objs, Func<object, object[]> rowGenerator)
         {
             var builder = new StringBuilder();
 
-            builder.AppendLine(GetExportHeaders());
+            builder.AppendLine(header);
             
-            foreach (var s in stocks)
+            foreach (var s in objs)
             {
-                builder.AppendLine(string.Join(",", GetExportParts(s.State)));
+                builder.AppendLine(string.Join(",", rowGenerator(s)));
             }
 
             return builder.ToString();
         }
 
-        public static object[] GetExportParts(SoldOptionState state)
+        private static object[] GetExportParts(SoldOptionState state)
         {
             return new object[]{
                 state.Ticker,
@@ -36,9 +52,31 @@ namespace core.Options
             };
         }
 
-        private static string GetExportHeaders()
+        private static string GetOptionsExportHeaders()
         {
             return "ticker,strike price,type,expiration,closed,amount,premium,spent,profit";
+        }
+
+        private static object[] GetExportParts(OwnedStockState state)
+        {
+            return new object[]{
+                state.Ticker,
+                state.Spent,
+                state.Earned,
+                state.Purchased.ToString("yyyy-MM-dd"),
+                state.Sold.ToString("yyyy-MM-dd"),
+                state.Profit
+            };
+        }
+
+        private static string GetStocksExportHeaders()
+        {
+            return "ticker,spent,earned,purchased,sold,profit";
+        }
+
+        public static string GenerateFilename(string exportType)
+        {
+            return exportType + "_" + DateTime.UtcNow.ToString($"yyyyMMdd_hhmss") + ".csv";
         }
     }
 }
