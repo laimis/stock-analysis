8b5e182438de83a71217030c88abc6b4d09225b3
Laimonas Simutis <lsimutis@mediafly.com>
2020-01-09 11:57:32 -0500

    list out env variables that are needed

--- dev.bat
+++ dev.bat
@@ -1,2 +1,10 @@
+REM fully functioning app needs teh following environment variables set:
+
+REM set IEXClientToken=.... # iex client token you can obtain from https://www.iexcloud.io/
+REM set DB_CNN=Server=...;Database=...;User id=...;password=... # database connection string
+
+REM set GoogleClientId=...  # for google oauth, get client id from google console
+REM set GoogleSecret=... # for google oauth, get secret from google console
+
 cd src\web
 dotnet watch run
