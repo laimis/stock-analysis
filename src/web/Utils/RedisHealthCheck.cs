c482d860cb213557ac554b795c32e1cf17fe6af8
Laimonas Simutis <lsimutis@mediafly.com>
2020-01-13 12:35:24 -0500

    adjust health checks to use redis

---
+++ src/web/Utils/RedisHealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace web.Utils
+{
+    public class RedisHealthCheck : IHealthCheck
+    {
+        private storage.redis.AggregateStorage _storage;
+
+        public RedisHealthCheck(storage.redis.AggregateStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            await _storage.DoHealthCheck();
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
