245bd1f95b7c8e53d0a62868ac5a27591dbabcc8
Laimonas Simutis <lsimutis@mediafly.com>
2020-01-15 10:42:58 -0500

    add tests for validation

---
+++ tests/coretests/Utils/NotInThePastTests.cs
@@ -0,0 +1,36 @@
+using System;
+using core.Utils;
+using Xunit;
+
+namespace coretests.Utils
+{
+    public class NotInThePastTests
+    {
+        [Fact]
+        public void InThePast_Fails()
+        {
+            var att = new NotInThePast();
+
+            Assert.False(att.IsValid(DateTimeOffset.UtcNow.AddDays(-1)));
+        }
+
+        [Fact]
+        public void InFuture_Succeeds()
+        {
+            var att = new NotInThePast();
+
+            Assert.True(att.IsValid(DateTimeOffset.UtcNow.AddDays(1)));
+        }
+
+        [Fact]
+        public void Message_Formatted()
+        {
+            var att = new NotInThePast();
+
+            var formatted = att.FormatErrorMessage("myparam");
+
+            Assert.Contains("myparam", formatted);
+            Assert.Contains("past", formatted);
+        }
+    }
+}
