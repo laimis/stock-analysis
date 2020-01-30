15374eb7909eb8126e4ffc71d6f243c9998bc556
Laimonas Simutis <lsimutis@mediafly.com>
2020-01-30 12:13:02 -0500

    increase entity versions

--- src/infrastructure/storage.shared/PortfolioStorage.cs
+++ src/infrastructure/storage.shared/PortfolioStorage.cs
@@ -12,9 +12,9 @@ namespace storage.shared
 {
     public class PortfolioStorage : IPortfolioStorage
     {
-        const string _stock_entity = "ownedstock2";
-        const string _option_entity = "soldoption2";
-        const string _note_entity = "note2";
+        const string _stock_entity = "ownedstock3";
+        const string _option_entity = "soldoption3";
+        const string _note_entity = "note3";
 
         private IAggregateStorage _aggregateStorage;
 
