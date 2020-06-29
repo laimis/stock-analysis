1189aaa73fb25a0b6c8b001be8e8143bb43bfb9b
Laimonas Simutis <laimis@gmail.com>
2020-06-29 07:13:51 -0400

    render alerts if they are present (#34)

---
+++ src/core/Alerts/Get.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using core.Shared;
+
+namespace core.Alerts
+{
+    public class Get
+    {
+        public class Query : RequestWithUserId<object>
+        {
+            public Query(Guid userId, string ticker) : base(userId)
+            {
+                this.Ticker = ticker;
+            }
+
+            public string Ticker { get; }
+        }
+
+        public class Handler : HandlerWithStorage<Query, object>
+        {
+            private IAlertsStorage _alertsStorage;
+
+            public Handler(
+                IPortfolioStorage storage,
+                IAlertsStorage alertsStorage) : base(storage)
+            {
+                _alertsStorage = alertsStorage;
+            }
+
+            public override async Task<object> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var alert = await _alertsStorage.GetAlert(request.Ticker, request.UserId);
+
+                if (alert == null)
+                {
+                    return null;
+                }
+
+                return new {
+                    ticker = alert.State.Ticker.Value,
+                    alert.State.Threshold
+                };
+            }
+        }
+    }
+}
