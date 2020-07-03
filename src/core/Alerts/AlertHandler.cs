a746c3386d4645cd1e19270862642a617bf9dca0
Laimonas Simutis <lsimutis@mediafly.com>
2020-07-02 20:51:34 -0400

    clean up alerts

---
+++ src/core/Alerts/AlertHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace core.Alerts
+{
+    public class AlertHandler :
+        MediatR.INotificationHandler<AlertPricePointRemoved>
+    {
+        private StockMonitorContainer _container;
+        private IAlertsStorage _storage;
+
+        public AlertHandler(IAlertsStorage storage, StockMonitorContainer container)
+        {
+            _container = container;
+            _storage = storage;
+        }
+
+        public Task Handle(AlertPricePointRemoved e, CancellationToken cancellationToken)
+        {
+            _container.Deregister(e.PricePointId);
+
+            return Task.CompletedTask;
+        }
+    }
+}
