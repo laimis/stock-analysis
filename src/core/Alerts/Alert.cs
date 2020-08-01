121f2a092aa333051ef09d51c0665418477ae416
Laimonas Simutis <lsimutis@mediafly.com>
2020-08-01 10:41:44 -0400

    Restructure alerts

--- src/core/Alerts/Alert.cs
+++ src/core/Alerts/Alert.cs
@@ -87,4 +87,18 @@ namespace core.Alerts
             }
         }
     }
+
+    public struct AlertPricePoint
+    {
+        public AlertPricePoint(Guid id, string description, double value)
+        {
+            Id = id;
+            Description = description;
+            Value = value;
+        }
+
+        public Guid Id { get; }
+        public string Description { get; }
+        public double Value { get; }
+    }
 }
