3ba525278f9231d79e467195b4104b28951d9462
Laimonas Simutis <lsimutis@mediafly.com>
2021-10-16 12:35:45 -0700

    ticker price debacle

--- src/core/Shared/Price.cs
+++ src/core/Shared/Price.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace core
 {
     public struct Price
@@ -5,6 +7,19 @@ namespace core
         public decimal Amount { get; }
 
         public Price(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public bool NotFound => Amount == 0;
+    }
+
+    [Obsolete("Used to recover note")]
+    public struct TickerPrice
+    {
+        public decimal Amount { get; }
+
+        public TickerPrice(decimal amount)
         {
             Amount = amount;
         }
