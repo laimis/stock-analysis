8e8f65b9e97855be92fd9ccd0276ea06559b8408
unknown <laimis@gmail.com>
2022-05-29 11:46:08 -0700

    add trading postion component, nicer styling

---
+++ src/web/ClientApp/src/app/trading/trading-position.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { StockTradingGridEntry } from '../services/stocks.service';
+
+@Component({
+  selector: 'app-trading-position',
+  templateUrl: './trading-position.component.html',
+  styleUrls: ['./trading-position.component.css']
+})
+export class TradingPositionComponent {
+    @Input()
+    p: StockTradingGridEntry
+
+    @Input()
+    stopLoss: number
+
+    @Input()
+    firstTarget: number
+
+    @Input()
+    rrTarget: number
+
+    gain() {
+        return (this.p.price - this.p.averageCost) / this.p.averageCost
+    }
+
+    stopNumber() {
+        return this.p.averageCost - this.p.averageCost * this.stopLoss
+    }
+
+    firstTargetNumber() {
+        return this.p.averageCost + this.p.averageCost * this.firstTarget
+    }
+
+    rrTargetNumber() {
+        return this.p.averageCost + this.p.averageCost * this.rrTarget
+    }
+
+}
+
