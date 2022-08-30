0f4e15090ddeafd96c7093bb8eca02a4ee75790b
unknown <laimis@gmail.com>
2022-08-30 13:35:00 -0700

    update simulator

--- src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-simulation.component.ts
+++ src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-simulation.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { StocksService, stocktransactioncommand } from '../../services/stocks.service';
-import { ActivatedRoute } from '@angular/router';
-import { tick } from '@angular/core/testing';
+import { StocksService } from '../../services/stocks.service';
+
+class StockTransaction {
+  numberOfShares: number
+  price: number
+  date: string
+  type: string
+}
 
 @Component({
   selector: 'stock-trading-simulation',
@@ -12,12 +17,23 @@ import { tick } from '@angular/core/testing';
 export class StockTradingSimulationComponent implements OnInit {
 
   stopPrice: number | null = null
-  positions: stocktransactioncommand[] = []
+  transactions: StockTransaction[] = []
   currentCost: number | null = null
-  profit: number | null = null
   ticker: string
 
-  constructor(private stocks:StocksService, private route: ActivatedRoute) { }
+  price: number | null = null
+  quantity: number | null = null
+  date: string
+
+  // dynamic calculated fields
+  cost : number = 0
+  profit: number = 0
+  averageCostPerShare: number = 0
+  numberOfShares: number = 0
+  riskedAmount: number = 0
+  unrealizedProfit: number = 0
+
+  constructor(private stocks:StocksService) { }
 
   ngOnInit(): void {
     var simulations = localStorage.getItem('simulations')
@@ -27,76 +43,110 @@ export class StockTradingSimulationComponent implements OnInit {
 
       this.ticker = data.ticker
       this.stopPrice = data.stopPrice
-      this.positions = data.positions
+      this.transactions = data.positions
       this.currentCost = data.currentCost
+      this.riskedAmount = data.riskedAmount
 
-      this.updateProfit()
+      this.runCalculations()
     }
   }
 
-  stockPurchased(stocktransactioncommand: stocktransactioncommand) {
+  buy() {
+    this.addTransaction('buy')
+  }
+  sell() {
+    this.addTransaction('sell')
+  }
 
-    this.ticker = stocktransactioncommand.ticker
-    
-    if (stocktransactioncommand.stopPrice) {
-      this.stopPrice = stocktransactioncommand.stopPrice
+  addTransaction(type:string) {
+
+    if (this.price && this.quantity) {
+      var transaction = {
+        numberOfShares: this.quantity,
+        price: this.price,
+        date: this.date,
+        type: type
+      }
+
+      if (this.currentCost == null) {
+        this.currentCost = transaction.price
+      }
+
+      this.transactions.push(transaction)
+
+      this.update()
     }
-
-    if (this.currentCost == null) {
-      this.currentCost = stocktransactioncommand.price
-    }
-
-    this.positions.push(stocktransactioncommand)
-
-    this.update()
   }
 
-  totalCost() {
-    return this.positions.reduce((acc, curr) => acc + curr.numberOfShares * curr.price, 0)
-  }
 
-  numberOfShares() {
-    return this.positions.reduce((acc, curr) => acc + curr.numberOfShares, 0)
-  }
-
-  averageCostPerShare() {
-    return this.totalCost() / this.numberOfShares()
-  }
-
-  riskedAmount() {
-    // average cost per share - stopPrice multipled by number of shares
-    return (this.averageCostPerShare() - this.stopPrice) * this.numberOfShares()
-  }
-
-  removePosition(index:number) {
-    this.positions.splice(index, 1)
+  removeTransaction(index:number) {
+    this.transactions.splice(index, 1)
     this.update()
   }
 
   reset() {
     localStorage.removeItem('simulations')
-    this.positions = []
+    this.transactions = []
     this.currentCost = null
     this.profit = null
     this.ticker = null
     this.stopPrice = null
+    this.riskedAmount = null
   }
 
   update() {
+    console.log('running update')
     var data = {
       stopPrice: this.stopPrice,
-      positions: this.positions,
+      positions: this.transactions,
       currentCost: this.currentCost,
-      ticker: this.ticker
+      ticker: this.ticker,
+      riskedAmount: this.riskedAmount
     }
 
     localStorage.setItem('simulations', JSON.stringify(data))
 
-    this.updateProfit()
+    this.runCalculations()
   }
 
-  updateProfit() {
-    this.profit = (this.currentCost - this.averageCostPerShare()) * this.numberOfShares()
+  calculateRiskedAmount() {
+    this.riskedAmount = Math.round((this.averageCostPerShare - this.stopPrice) * this.numberOfShares * 100) / 100
+    this.update()
+  }
+
+  runCalculations() {
+    var slots : number[] = []
+    var cost : number = 0
+    var profit : number = 0
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type == 'buy') {
+        for (let i = 0; i < transaction.numberOfShares; i++) {
+          slots.push(transaction.price)
+          cost += transaction.price
+          this.numberOfShares++
+        }
+      } else {
+        // remove quantity number of slots from the beginning of an array
+        var removed = slots.splice(0, transaction.numberOfShares)
+        removed.forEach(removedElement => {
+          profit += transaction.price - removedElement
+          cost -= removedElement
+          this.numberOfShares--
+        })
+      }
+    });
+
+    // calculate average cost per share using slots
+    this.averageCostPerShare = slots.reduce((acc, curr) => acc + curr, 0) / slots.length
+    this.cost = cost
+    this.profit = profit
+    
+
+    this.unrealizedProfit = slots.reduce((acc, curr) => acc + this.currentCost - curr, 0)
+
+    console.log(slots)
+    console.log(profit)
   }
 
 }
