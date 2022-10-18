d640288e52ea0f82d7b3e7c636ad8d9a57c20127
unknown <laimis@gmail.com>
2022-10-18 12:24:52 -0700

    target fx change

--- src/core/Account/IAccountStorage.cs
+++ src/core/Account/IAccountStorage.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -6,15 +7,16 @@ namespace core.Account
 {
     public interface IAccountStorage
     {
-        Task<T> ViewModel<T>(Guid userId);
+        Task<T?> ViewModel<T>(Guid userId);
         Task SaveViewModel<T>(T user, Guid userId);
 
-        Task<User> GetUserByEmail(string emailAddress);
-        Task<User> GetUser(Guid userId);
+        Task<User?> GetUserByEmail(string emailAddress);
+        Task<User?> GetUser(Guid userId);
         Task Save(User u);
         Task Delete(User u);
         Task SaveUserAssociation(ProcessIdToUserAssociation r);
-        Task<ProcessIdToUserAssociation> GetUserAssociation(Guid guid);
+        Task<ProcessIdToUserAssociation?> GetUserAssociation(Guid guid);
         Task<IEnumerable<(string email, string id)>> GetUserEmailIdPairs();
     }
 }
+#nullable restore
--- src/infrastructure/storage.memory/AccountStorage.cs
+++ src/infrastructure/storage.memory/AccountStorage.cs
@@ -5,7 +5,7 @@ using MediatR;
 namespace storage.memory;
 public class AccountStorage : MemoryAggregateStorage, IAccountStorage
 {
-    private static readonly Dictionary<Guid, User> _users = new Dictionary<Guid, User>();
+    private static readonly Dictionary<Guid, User?> _users = new Dictionary<Guid, User?>();
     private static readonly Dictionary<Guid, ProcessIdToUserAssociation> _associations = new Dictionary<Guid, ProcessIdToUserAssociation>();
     private static readonly Dictionary<Guid, object> _viewModels = new Dictionary<Guid, object>();
 
@@ -22,7 +22,7 @@ public class AccountStorage : MemoryAggregateStorage, IAccountStorage
     public Task<User?> GetUser(Guid userId)
     {
         _users.TryGetValue(userId, out User? u);
-        return Task.FromResult(u);
+        return Task<User?>.FromResult(u);
     }
 
     public Task<ProcessIdToUserAssociation?> GetUserAssociation(Guid guid) =>
@@ -34,13 +34,13 @@ public class AccountStorage : MemoryAggregateStorage, IAccountStorage
 
     public Task<User?> GetUserByEmail(string emailAddress)
     {
-        var user = _users.Values.FirstOrDefault(u => u.State.Email == emailAddress);
+        var user = _users.Values.FirstOrDefault(u => u?.State?.Email == emailAddress);
         return Task.FromResult(user);
     }
 
     public Task<IEnumerable<(string email, string id)>> GetUserEmailIdPairs() =>
         Task.FromResult(
-            _users.Values.Select(u => (u.State.Email, u.Id.ToString()))
+            _users.Values.Where(u => u != null).Select(u => (u!.State.Email, u.Id.ToString()))
         );
 
     public async Task Save(User u)
