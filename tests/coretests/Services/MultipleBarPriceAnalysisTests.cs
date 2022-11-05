54e817cfdb1ed957e0a3ff6dc1ebbe9829ffd2cf
unknown <laimis@gmail.com>
2022-11-05 08:54:40 -0700

    init volume numbers

--- tests/coretests/Services/MultipleBarPriceAnalysisTests.cs
+++ tests/coretests/Services/MultipleBarPriceAnalysisTests.cs
@@ -13,9 +13,10 @@ namespace coretests.Services
 
         public MultipleBarPriceAnalysisTests()
         {
+            var rand = new Random();
             var start = new DateTime(2020, 1, 1, 1, 1, 1);
             var historcalPrices = Enumerable.Range(1, 30)
-                .Select(n => new PriceBar { Close = n, Date = start.AddDays(n).ToString()})
+                .Select(n => new PriceBar { Close = n, Date = start.AddDays(n).ToString(), Volume = rand.Next(1000000) })
                 .ToArray();
 
             _outcomes = MultipleBarPriceAnalysis.Run(10, historcalPrices);
