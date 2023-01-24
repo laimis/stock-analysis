9f08ae648f53abbc9d021c817c82706298922ab8
unknown <laimis@gmail.com>
2023-01-24 12:52:07 -0800

    we don't care about positions without sell orders anymore

--- src/core/Stocks/Services/Analysis/PositionAnalysisOutcomeEvaluation.cs
+++ src/core/Stocks/Services/Analysis/PositionAnalysisOutcomeEvaluation.cs
@@ -21,18 +21,6 @@ namespace core.Stocks.Services.Analysis
                         t.outcomes.Any(o => o.key == PortfolioAnalysisKeys.PercentToStopLoss && o.value >= PercentToStopThreshold))
                     .ToList()
             );
-
-            // stocks that have stops but don't have pending sell orders
-            yield return new AnalysisOutcomeEvaluation(
-                "Positions without sell orders",
-                OutcomeType.Negative,
-                PortfolioAnalysisKeys.PercentToStopLoss,
-                tickerOutcomes
-                    .Where(t => 
-                        t.outcomes.Any(o => o.key == PortfolioAnalysisKeys.PercentToStopLoss && o.value != PercentToStopNotSetValue)
-                        && orders.Any(o => o.Ticker == t.ticker && o.Type == "SELL" && o.Status != "FILLED") == false)
-                    .ToList()
-            );
         }
     }
 }
