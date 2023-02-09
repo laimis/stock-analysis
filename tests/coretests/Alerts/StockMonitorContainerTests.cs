adfb983cc33c0c143d9305ad0a8b4e27dd33ec57
unknown <laimis@gmail.com>
2023-02-09 14:23:33 -0800

    fix tests

--- tests/coretests/Alerts/StockMonitorContainerTests.cs
+++ tests/coretests/Alerts/StockMonitorContainerTests.cs
@@ -7,11 +7,12 @@ namespace coretests.Alerts
 {
     public class StockMonitorContainerTests
     {
+        private Guid _userId;
         private StockAlertContainer _uat;
 
         public StockMonitorContainerTests()
         {
-            var userId = Guid.NewGuid();
+            _userId = Guid.NewGuid();
             
             _uat = new StockAlertContainer();
 
@@ -20,7 +21,7 @@ namespace coretests.Alerts
                     stopPrice: 105,
                     ticker: "AMD",
                     when: DateTimeOffset.Now,
-                    userId: userId
+                    userId: _userId
                 );
 
             _uat.Register(alert);
@@ -28,10 +29,13 @@ namespace coretests.Alerts
         }
 
         [Fact]
-        public void AlertsNotEmpty() => Assert.NotEmpty(_uat.Alerts);
+        public void Alerts_ForUser_NotEmpty() => Assert.NotEmpty(_uat.GetAlerts(_userId));
 
         // checks that registering is idempotent
         [Fact]
-        public void MonitorsCountIsCorrect() => Assert.Single(_uat.Alerts);
+        public void Alerts_ForUser_OnlyOne() => Assert.Single(_uat.GetAlerts(_userId));
+
+        [Fact]
+        public void Alerts_ForDifferentUser_Empty() => Assert.Empty(_uat.GetAlerts(Guid.NewGuid()));
     }
 }
