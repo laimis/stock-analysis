a1ab287b2603def4d9f12a898647f7d63609f9f4
unknown <laimis@gmail.com>
2023-03-08 13:29:40 -0800

    fix issues

--- tests/infrastructuretests/secedgartests/EdgarClientTests.cs
+++ tests/infrastructuretests/secedgartests/EdgarClientTests.cs
@@ -8,8 +8,8 @@ public class EdgarClientTests
     [Fact]
     public async Task TestAsync()
     {
-        var client = new EdgarClient("NGTD/1.0");
-        var filings = await client.GetFilings("AAPL");
-        Assert.True(filings.filings.Count > 0);
+        var client = new EdgarClient(null, "NGTD/1.0");
+        var response = await client.GetFilings("AAPL");
+        Assert.True(response.Success.filings.Count > 0);
     }
 }
