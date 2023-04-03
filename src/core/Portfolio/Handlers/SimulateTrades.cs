085fa9691fa531a66d24c710865318ae50348c39
unknown <laimis@gmail.com>
2023-04-03 11:41:39 -0700

    remove unused using

--- src/core/Portfolio/Handlers/SimulateTrades.cs
+++ src/core/Portfolio/Handlers/SimulateTrades.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using core.Account;
-using core.Portfolio.Views;
 using core.Shared;
 using core.Shared.Adapters.Brokerage;
 using core.Shared.Adapters.CSV;
