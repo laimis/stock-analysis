e060e3607abb449419389959b39942ebae30ff47
Laimonas Simutis <laimis@gmail.com>
2023-08-21 12:27:37 -0700

    fix logic for scheduling stop loss monitoring, adjust tests

--- tests/coretests/Alerts/ScanSchedulingTests.cs
+++ tests/coretests/Alerts/ScanSchedulingTests.cs
@@ -26,13 +26,13 @@ namespace coretests.Alerts
         }
 
         [Theory]
-        [InlineData("2023-02-28T00:00:00Z", "2023-02-28T14:30:00.0000000+00:00")] // before market open
-        [InlineData("2023-02-28T21:46:00Z", "2023-03-01T14:30:00.0000000+00:00")] // after market close
+        [InlineData("2023-02-28T00:00:00Z", "2023-02-28T14:45:00.0000000+00:00")] // before market open
+        [InlineData("2023-02-28T21:46:00Z", "2023-03-01T14:45:00.0000000+00:00")] // after market close
         [InlineData("2023-02-28T14:45:00Z", "2023-02-28T14:50:00.0000000+00:00")] // 9:45am et
         [InlineData("2023-02-28T15:45:00Z", "2023-02-28T15:50:00.0000000+00:00")] // 10:45am et
         [InlineData("2023-02-28T20:30:00Z", "2023-02-28T20:35:00.0000000+00:00")] // 3:30pm et
-        [InlineData("2023-02-28T21:30:00Z", "2023-03-01T14:30:00.0000000+00:00")] // 4:30pm et
-        [InlineData("2023-03-04T00:00:00Z", "2023-03-06T14:30:00.0000000+00:00")] // friday evening et
+        [InlineData("2023-02-28T21:30:00Z", "2023-03-01T14:45:00.0000000+00:00")] // 4:30pm et
+        [InlineData("2023-03-04T00:00:00Z", "2023-03-06T14:45:00.0000000+00:00")] // friday evening et
         public void GetNextStopLossMonitor_Works(string inputUtc, string expectedUtc)
         {
             var time = DateTimeOffset.Parse(inputUtc, null, System.Globalization.DateTimeStyles.AssumeUniversal);
