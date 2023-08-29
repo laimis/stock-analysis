8e555d8c9f92d27f6be5d6dccb224cc1f493eef2
Laimonas Simutis <laimis@gmail.com>
2023-08-29 09:53:16 -0700

    rename candlestick chart component

---
+++ src/web/ClientApp/src/app/shared/candlestick-chart/candlestick-chart.component.ts
@@ -0,0 +1,122 @@
+import { Component, Input, OnDestroy } from '@angular/core';
+import { Prices } from 'src/app/services/stocks.service';
+import { IChartApi, PriceLineOptions, createChart } from 'lightweight-charts';
+
+@Component({
+  selector: 'app-candlestick-chart',
+  templateUrl: './candlestick-chart.component.html',
+  styleUrls: ['./candlestick-chart.component.css']
+})
+export class CandlestickChartComponent implements OnDestroy {
+  chart: IChartApi;
+
+  ngOnDestroy(): void {
+    this.removeChart();
+  }
+
+  private removeChart() {
+    if (this.chart) {
+      this.chart.remove();
+    }
+  }
+
+  renderChart() {
+
+    console.log('rendering chart')
+    console.log(this.buyDates)
+    console.log(this.sellDates)
+
+    this.removeChart();
+
+    this.chart = createChart(
+      document.getElementById('chart'),
+      { height: this.chartHeight }
+    );
+
+    let createLineData = (sma, interval, priceBars) => {
+      return sma.values
+        .filter(v => v != null)
+        .map( (p, index) => ({ value: p, time: priceBars[index + interval].time }));
+    }
+
+    let toCandleStickData = (p) => {
+      let isBuyPoint = this.buyDates && this.buyDates.includes(p.dateStr)
+      let isSellPoint = this.sellDates && this.sellDates.includes(p.dateStr)
+      let color = isBuyPoint ? 'black' : isSellPoint ? 'orange' : null
+      return { time: p.dateStr, open: p.open, high: p.high, low: p.low, close: p.close, color: color }
+    }
+
+    let addLineSeries = (sma, color, interval, priceBars) => {
+      const smaSeries = this.chart.addLineSeries({ color: color, lineWidth: 1, crosshairMarkerVisible: false });
+      let smaLineData = createLineData(sma, interval, priceBars)
+      smaSeries.setData(smaLineData);
+    }
+
+    const barSeries = this.chart.addCandlestickSeries();
+    let priceBars = this._prices.prices.map(toCandleStickData)
+    barSeries.setData(priceBars);
+    
+    if (this.averageBuyPrice) {
+      var buyPrice : PriceLineOptions = {
+        'title': 'avg cost',
+        price: this.averageBuyPrice,
+        color: 'blue',
+        lineWidth: 1,
+        lineVisible: true,
+        axisLabelColor: 'blue',
+        axisLabelTextColor: 'white',
+        lineStyle: 0,
+        axisLabelVisible: true
+      };
+
+      barSeries.createPriceLine(buyPrice);
+    }
+
+    if (this.stopPrice) {
+      var stopPrice : PriceLineOptions = {
+        'title': 'stop',
+        price: this.stopPrice,
+        color: 'red',
+        lineWidth: 1,
+        lineVisible: true,
+        axisLabelColor: 'red',
+        axisLabelTextColor: 'white',
+        lineStyle: 0,
+        axisLabelVisible: true
+      };
+
+      barSeries.createPriceLine(stopPrice);
+    }
+
+    addLineSeries(this._prices.sma.sma20, 'red', 20, priceBars);
+    addLineSeries(this._prices.sma.sma50, 'green', 50, priceBars);
+    addLineSeries(this._prices.sma.sma150, 'lightblue', 150, priceBars);
+    addLineSeries(this._prices.sma.sma200, 'blue', 200, priceBars);
+    
+    this.chart.timeScale().fitContent();
+  }
+
+  @Input()
+  chartHeight: number = 400;
+  private _prices: Prices;
+
+  @Input()
+  set prices(value: Prices) {
+    if (value) {
+      this._prices = value;
+      this.renderChart();
+    }
+  }
+
+  @Input()
+  averageBuyPrice = null;
+
+  @Input()
+  stopPrice = null;
+
+  @Input()
+  buyDates = null;
+
+  @Input()
+  sellDates = null;
+}
