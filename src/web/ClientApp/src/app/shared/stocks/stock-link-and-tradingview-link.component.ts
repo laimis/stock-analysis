d7d65e0c7e0f06703a9b716f132260482c0f8be7
Laimonas Simutis <laimis@gmail.com>
2023-10-30 09:33:08 -0700

    creating a component that hosts stock and chart links in one place

---
+++ src/web/ClientApp/src/app/shared/stocks/stock-link-and-tradingview-link.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";
+
+
+@Component({
+  selector: 'app-stock-link-and-tradingview-link',
+  templateUrl: './stock-link-and-tradingview-link.component.html'
+})
+export class StockLinkAndTradingviewLinkComponent {
+
+  @Input()
+  public ticker : string;
+
+  @Input()
+  public openInNewTab : boolean = false;
+}
+
