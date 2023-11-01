4089cea7e37a45efb0c8e853c80ff9b1445b5bb5
Laimonas Simutis <laimis@gmail.com>
2023-11-01 11:12:34 -0700

    refactor stock search functionality to allow to use keys in addition to mouse

--- src/web/ClientApp/src/app/stocks/stock-search/stock-search.component.ts
+++ src/web/ClientApp/src/app/stocks/stock-search/stock-search.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Subject, Observable} from 'rxjs';
+import {debounceTime, switchMap} from 'rxjs/operators';
 import {StockSearchResult, StocksService} from "../../services/stocks.service";
 
 @Component({
@@ -12,60 +12,83 @@ export class StockSearchComponent implements OnInit {
 
   @Input() label: string = "Search for securities using ticker or name"
   @Input() cssClass: string = "form-control"
+
   @Input()
   set ticker(value: string) {
     this.selectedValue = value
   }
+
   @Input() placeholder: string
   @Output() tickerSelected = new EventEmitter<string>();
 
   selectedValue: string = null
 
-  public searchResults$: Observable<StockSearchResult[]>;
+  public searchResults: Observable<StockSearchResult[]>;
   private searchTerms = new Subject<string>();
-  showSuggestions: boolean;
+  searchResultsSubscribedArray: StockSearchResult[] = []
+  highlightedIndex = -1;
 
   constructor(
-    private stocks : StocksService
-  ) { }
+    private stocks: StocksService
+  ) {
+  }
 
   ngOnInit() {
 
-    this.searchResults$ = this.searchTerms.pipe(
+    this.searchResults = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
       debounceTime(300),
 
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
+      // ignore new term if same as previous term -- commented this one out because
+      // if a user went back and used delete to go back to their previous query, this
+      // filter would consider it duplicate and not pass it on
+      // distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.stocks.search(term)),
-
-      tap(r => this.reportResults(r))
     );
-  }
 
-  reportResults(arr:object[]) {
-    console.log("reportResults: " + arr.length)
-  }
-
-  search(term:string) {
-    console.log("search: " + term)
-    this.showSuggestions = true
-    this.searchTerms.next(term);
+    this.searchResults.subscribe(value => {
+      console.log("searchResults.subscribe: " + value.length)
+      this.searchResultsSubscribedArray = value
+    })
   }
 
   loseFocus() {
     this.searchTerms.next('')
-    if (!this.selectedValue)
-    {
+    if (!this.selectedValue) {
     }
   }
 
-  clicked(ticker:string) {
+  clicked(ticker: string) {
     console.log("clicked: " + ticker)
     this.selectedValue = ticker
     this.tickerSelected.emit(ticker)
-    this.showSuggestions = false
+  }
+
+  onModelChange($event: string) {
+    console.log("onModelChange: " + $event)
+    this.searchTerms.next($event)
+  }
+
+  onKeyDown($event: KeyboardEvent) {
+
+    if (this.searchResultsSubscribedArray.length == 0) {
+      return
+    }
+
+    if ($event.key === 'ArrowUp' && this.highlightedIndex > 0) {
+      this.highlightedIndex--;
+    }
+
+    if ($event.key === 'ArrowDown' && this.highlightedIndex < this.searchResultsSubscribedArray.length - 1) {
+      this.highlightedIndex++;
+    }
+
+    if ($event.key === 'Enter' && this.highlightedIndex >= 0) {
+      let ticker = this.searchResultsSubscribedArray[this.highlightedIndex].symbol
+      this.loseFocus()
+      this.clicked(ticker)
+    }
   }
 }
