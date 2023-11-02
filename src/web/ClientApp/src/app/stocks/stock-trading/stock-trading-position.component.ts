0f3b8d235d672f951bf82e88554852a6f374828f
Laimonas Simutis <laimis@gmail.com>
2023-11-02 14:05:34 -0700

    cleanup mobile rendering for recording trades

--- src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-position.component.ts
+++ src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-position.component.ts
@@ -1,8 +1,14 @@
-import { Component, Input } from '@angular/core';
-import { BrokerageOrder, PositionEvent, PositionInstance, StocksService, StrategyProfitPoint } from '../../services/stocks.service';
-import { Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
-import { GetErrors, GetStrategies, toggleVisuallyHidden } from 'src/app/services/utils';
+import {Component, Input} from '@angular/core';
+import {
+  BrokerageOrder,
+  PositionEvent,
+  PositionInstance,
+  StocksService,
+  StrategyProfitPoint
+} from '../../services/stocks.service';
+import {Output} from '@angular/core';
+import {EventEmitter} from '@angular/core';
+import {GetErrors, GetStrategies, toggleVisuallyHidden} from 'src/app/services/utils';
 
 @Component({
   selector: 'app-stock-trading-position',
@@ -10,177 +16,175 @@ import { GetErrors, GetStrategies, toggleVisuallyHidden } from 'src/app/services
   styleUrls: ['./stock-trading-position.component.css']
 })
 export class StockTradingPositionComponent {
-    candidateRiskAmount: number = 0
-    candidateStopPrice: number = 0
-    _position: PositionInstance;
-    numberOfProfitPoints: number = 4
+  candidateRiskAmount: number = 0
+  candidateStopPrice: number = 0
+  _position: PositionInstance;
+  numberOfProfitPoints: number = 4
 
-    positionProfitPoints : StrategyProfitPoint[] = []
-    positionStrategy: string = null
-    positionOrders: BrokerageOrder[] = [];
-    allOrders: BrokerageOrder[] = [];
-    strategies: { key: string; value: string; }[];
+  positionProfitPoints: StrategyProfitPoint[] = []
+  positionStrategy: string = null
+  positionOrders: BrokerageOrder[] = [];
+  allOrders: BrokerageOrder[] = [];
+  strategies: { key: string; value: string; }[];
 
-    @Input()
-    set position(v:PositionInstance) {
-        this._position = v
+  @Input()
+  set position(v: PositionInstance) {
+    this._position = v
 
-        if (this._position) {
-            this.positionStrategy = v.labels.find(l => l.key == "strategy")?.value
-            this.positionProfitPoints = []
-            this.setCandidateValues()
-            this.updatePositionOrders()
+    if (this._position) {
+      this.positionStrategy = v.labels.find(l => l.key == "strategy")?.value
+      this.positionProfitPoints = []
+      this.setCandidateValues()
+      this.updatePositionOrders()
+    }
+  }
+
+  @Input()
+  set orders(value: BrokerageOrder[]) {
+    this.allOrders = value;
+    this.updatePositionOrders();
+  }
+
+  updatePositionOrders() {
+    if (!this._position) {
+      return
+    }
+
+    if (!this.allOrders) {
+      return
+    }
+
+    this.positionOrders = this.allOrders.filter(o => o.ticker == this._position.ticker)
+  }
+
+  @Output()
+  positionDeleted = new EventEmitter()
+
+  // constructor that takes stock service
+  constructor(
+    private stockService: StocksService
+  ) {
+    this.strategies = GetStrategies()
+  }
+
+  fetchProfitPoints() {
+    this.stockService.getStrategyProfitPoints(
+      this._position.ticker,
+      this._position.positionId,
+      this.numberOfProfitPoints).subscribe(
+      (profitPoints) => {
+        this.positionProfitPoints = profitPoints
+      }
+    )
+  }
+
+  setCandidateValues() {
+    this.candidateRiskAmount = this._position.riskedAmount
+    this.candidateStopPrice = this._position.stopPrice
+  }
+
+  recalculateRiskAmount() {
+    var newRiskAmount = (this._position.averageCostPerShare - this.candidateStopPrice) * this._position.numberOfShares
+    this.candidateRiskAmount = newRiskAmount
+    this._position.riskedAmount = newRiskAmount
+    return false
+  }
+
+  setStopPrice() {
+    this.stockService.setStopPrice(this._position.ticker, this.candidateStopPrice).subscribe(
+      (_) => {
+        this._position.stopPrice = this.candidateStopPrice
+      }
+    )
+  }
+
+  deleteStopPrice() {
+    if (confirm("Are you sure you want to delete the stop price?")) {
+      this.stockService.deleteStopPrice(this._position.ticker).subscribe(
+        (_) => {
+          this._position.stopPrice = null
+          this._position.riskedAmount = null
         }
+      )
     }
 
-    @Input()
-    set orders(value:BrokerageOrder[]) {
-        this.allOrders = value;
-        this.updatePositionOrders();
-    }
+    return false
+  }
 
-    updatePositionOrders() {
-        if (!this._position) {
-            return
+  setRiskAmount() {
+    if (confirm("Are you sure you want to set the risk amount?")) {
+      this.stockService.setRiskAmount(this._position.ticker, this._position.positionId, this.candidateRiskAmount).subscribe(
+        (_) => {
+          this._position.riskedAmount = this.candidateRiskAmount
         }
+      )
+    }
+  }
 
-        if (!this.allOrders) {
-            return
-        }
+  getCssClassForEvent(e: PositionEvent) {
+    return "event-" + e.type
+  }
 
-        this.positionOrders = this.allOrders.filter(o => o.ticker == this._position.ticker)
+  deletePosition() {
+    // prompt user to confirm
+    if (confirm("Are you sure you want to delete this position?")) {
+      this.stockService.deletePosition(this._position.ticker, this._position.positionId)
+        .subscribe(
+          (_) => {
+            this._position = null
+            this.positionDeleted.emit()
+          })
+    }
+  }
+
+
+  deleteTransaction(transactionId: string) {
+    if (confirm("are you sure you want to delete the transaction?")) {
+      this.stockService.deleteStockTransaction(this._position.ticker, transactionId)
+        .subscribe(
+          _ => {
+            // refresh UI somehow here, tbd
+          }, (err) => {
+            var errors = GetErrors(err)
+            alert("Error deleting transaction: " + errors.join(", "))
+          })
+    }
+  }
+
+  clearStrategy() {
+    this.stockService.deleteLabel(this._position.ticker, this._position.positionId, "strategy").subscribe(
+      (r) => {
+      },
+      (err) => {
+        alert("Error clearing strategy")
+      }
+    )
+
+    return false
+  }
+
+  setStrategy(strategy: string) {
+    if (!strategy) {
+      alert("Please select strategy")
+      return
     }
 
-    @Output()
-    positionDeleted = new EventEmitter()
-
-    // constructor that takes stock service
-    constructor(
-        private stockService:StocksService
-    ) {
-        this.strategies = GetStrategies()
+    let label = {
+      key: "strategy",
+      value: strategy
     }
 
-    fetchProfitPoints() {
-        this.stockService.getStrategyProfitPoints(
-          this._position.ticker,
-          this._position.positionId,
-          this.numberOfProfitPoints).subscribe(
-            (profitPoints) => {
-                this.positionProfitPoints = profitPoints
-            }
-        )
-    }
+    this.stockService.setLabel(this._position.ticker, this._position.positionId, label).subscribe(
+      (r) => {
+      },
+      (err) => {
+        alert("Error setting strategy")
+      }
+    )
+  }
 
-    setCandidateValues() {
-        this.candidateRiskAmount = this._position.riskedAmount
-        this.candidateStopPrice = this._position.stopPrice
-    }
-
-    recalculateRiskAmount() {
-        var newRiskAmount = (this._position.averageCostPerShare - this.candidateStopPrice) * this._position.numberOfShares
-        this.candidateRiskAmount = newRiskAmount
-        this._position.riskedAmount = newRiskAmount
-        return false
-    }
-
-    setStopPrice() {
-        this.stockService.setStopPrice(this._position.ticker, this.candidateStopPrice).subscribe(
-            (_) => {
-                this._position.stopPrice = this.candidateStopPrice
-            }
-        )
-    }
-
-    deleteStopPrice() {
-        if (confirm("Are you sure you want to delete the stop price?"))
-        {
-            this.stockService.deleteStopPrice(this._position.ticker).subscribe(
-                (_) => {
-                    this._position.stopPrice = null
-                    this._position.riskedAmount = null
-                }
-            )
-        }
-
-        return false
-    }
-
-    setRiskAmount() {
-        if (confirm("Are you sure you want to set the risk amount?")) {
-            this.stockService.setRiskAmount(this._position.ticker, this._position.positionId, this.candidateRiskAmount).subscribe(
-                (_) => {
-                    this._position.riskedAmount = this.candidateRiskAmount
-                }
-            )
-        }
-    }
-
-    getCssClassForEvent(e:PositionEvent) {
-        return "event-" + e.type
-    }
-
-    deletePosition() {
-        // prompt user to confirm
-        if (confirm("Are you sure you want to delete this position?")) {
-            this.stockService.deletePosition(this._position.ticker, this._position.positionId)
-                .subscribe(
-                (_) => {
-                    this._position = null
-                    this.positionDeleted.emit()
-                })
-        }
-    }
-
-
-    deleteTransaction(transactionId:string)
-    {
-        if (confirm("are you sure you want to delete the transaction?")) {
-            this.stockService.deleteStockTransaction(this._position.ticker, transactionId)
-                .subscribe(
-                    _ => {
-                        // refresh UI somehow here, tbd
-                    }, (err) => {
-                        var errors = GetErrors(err)
-                        alert("Error deleting transaction: " + errors.join(", "))
-                    })
-        }
-    }
-
-    clearStrategy() {
-        this.stockService.deleteLabel(this._position.ticker, this._position.positionId, "strategy").subscribe(
-            (r) => {
-            },
-            (err) => {
-                alert("Error clearing strategy")
-            }
-        )
-
-        return false
-    }
-
-    setStrategy(strategy:string) {
-        if (!strategy) {
-            alert("Please select strategy")
-            return
-        }
-
-        let label = {
-            key: "strategy",
-            value: strategy
-        }
-
-        this.stockService.setLabel(this._position.ticker, this._position.positionId, label).subscribe(
-            (r) => {
-            },
-            (err) => {
-                alert("Error setting strategy")
-            }
-        )
-    }
-
-    toggleVisibility(elem) {
-        toggleVisuallyHidden(elem)
-    }
+  toggleVisibility(elem) {
+    toggleVisuallyHidden(elem)
+  }
 }
 
