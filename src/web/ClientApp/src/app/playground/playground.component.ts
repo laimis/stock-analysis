e0c5a269ddda8717ef853b66bb2013c6ccedfc8c
Laimonas Simutis <laimis@gmail.com>
2023-11-07 14:08:15 -0800

    clean up peaks/valleys code

--- src/web/ClientApp/src/app/playground/playground.component.ts
+++ src/web/ClientApp/src/app/playground/playground.component.ts
@@ -11,6 +11,7 @@ import {
 import {GetErrors} from "../services/utils";
 import {green, red} from "../shared/candlestick-chart/candlestick-chart.component";
 
+type gradient = {delta:number,index:number,bar:PriceBar}
 
 @Component({
   selector: 'app-playground',
@@ -45,11 +46,11 @@ export class PlaygroundComponent implements OnInit {
       result => {
         this.prices = result
 
-        let peaksAndTroughs = this.calculatePeaksAndTroughs(result.prices);
+        let peaksAndTroughs = this.calculatePeaksAndValleys(result.prices);
 
         let markers = []
         peaksAndTroughs.peaks.forEach(p => markers.push(p))
-        peaksAndTroughs.troughs.forEach(t => markers.push(t))
+        peaksAndTroughs.valleys.forEach(t => markers.push(t))
 
         this.chartInfo = {
           ticker: this.tickers[0],
@@ -68,55 +69,50 @@ export class PlaygroundComponent implements OnInit {
   }
 
 
-  calculatePeaksAndTroughs(prices:PriceBar[]) {
-    let diff = []
+  calculatePeaksAndValleys(prices:PriceBar[]) {
+    let diffs : gradient[] = []
 
     for (let i = 0; i < prices.length - 1; i++) {
-      diff.push(prices[i+1].close - prices[i].close);
+      let diff = prices[i+1].close - prices[i].close
+      let val : gradient = {bar: prices[i], delta: diff, index: i}
+      diffs.push(val);
     }
 
-    let smoothed = []
-    for (let i = 0; i < diff.length - 1; i++) {
+    let smoothed : gradient[] = []
+    for (let i = 0; i < diffs.length - 1; i++) {
       if (i == 0) {
-        smoothed.push((diff[i] + diff[i+1]) / 2);
+        let smoothedDiff = (diffs[i].delta + diffs[i+1].delta) / 2
+        smoothed.push({bar: diffs[i].bar, delta: smoothedDiff, index: i});
         continue;
       }
-      smoothed.push((diff[i-1] + diff[i] + diff[i+1]) / 3);
+      let smoothedDiff = (diffs[i-1].delta + diffs[i].delta + diffs[i+1].delta) / 3
+      smoothed.push({bar: diffs[i].bar, delta: smoothedDiff, index: i});
     }
 
-    let peakIndexes = []
-    let troughIndexes = []
+    let peakGradients: gradient[] = []
+    let valleyGradients: gradient[] = []
 
     for (let i = 0; i < smoothed.length - 1; i++) {
-      if (smoothed[i] > 0 && smoothed[i+1] < 0) {
-        peakIndexes.push(i);
-      } else if (smoothed[i] < 0 && smoothed[i+1] > 0) {
-        troughIndexes.push(i);
+      if (smoothed[i].delta > 0 && smoothed[i+1].delta < 0) {
+        peakGradients.push(smoothed[i+1]);
+      } else if (smoothed[i].delta < 0 && smoothed[i+1].delta > 0) {
+        valleyGradients.push(smoothed[i+1]);
       }
     }
 
-    let peaks = peakIndexes
-      .map(i => {
-        const marker: ChartMarker = {
-          label: 'peak',
-          date: prices[i + 1].dateStr,
-          color: red,
-          shape: 'arrowDown'
-        }
-        return marker
-      })
+    let toChartMarker = (g:gradient) => {
+      const marker: ChartMarker = {
+        label: g.bar.close.toFixed(2), //g.delta.toFixed(2),
+        date: g.bar.dateStr,
+        color: g.delta > 0 ? green : red,
+        shape:  g.delta > 0 ? 'arrowUp' : 'arrowDown'
+      }
+      return marker
+    }
 
-    let troughs = troughIndexes
-      .map(i => {
-        const marker: ChartMarker = {
-          label: 'trough',
-          date: prices[i + 1].dateStr,
-          color: green,
-          shape: 'arrowUp'
-        }
-        return marker
-      })
+    let peaks = peakGradients.map(toChartMarker)
+    let valleys = valleyGradients.map(toChartMarker)
 
-    return {peaks, troughs};
+    return {peaks, valleys};
   }
 }
