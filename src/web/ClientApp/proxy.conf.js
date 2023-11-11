47ed3b5f22c5078ba55655ecd36b492732cff28a
Laimonas Simutis <laimis@gmail.com>
2023-11-11 08:07:33 -0800

    updates to get the build to work

--- src/web/ClientApp/proxy.conf.js
+++ src/web/ClientApp/proxy.conf.js
@@ -6,7 +6,9 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api",
+      "/tradingview.png",
+      "/styles.css"
     ],
     target: target,
     secure: false,
