354183247aae4606dd68e7f8ac9177a732c94545
Laimonas Simutis <laimis@gmail.com>
2023-11-12 08:39:30 -0800

    hardcoding api port for cases when I want to run angular app myself in a separate window

--- src/web/ClientApp/proxy.conf.js
+++ src/web/ClientApp/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:49213';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5002';
 
 const PROXY_CONFIG = [
   {
