1bd464ca98e94d4ee5793ee0a63ca9420b88a248
Laimonas Simutis <laimis@gmail.com>
2023-11-13 09:29:21 -0800

    fix bug where selecting market order does not auto select duration

---
+++ src/web/ClientApp/src/app/services/brokerage.service.ts
@@ -0,0 +1,14 @@
+
+export enum BrokerageOrderType {
+  Market = 'Market',
+  Limit = 'Limit',
+  Stop = 'Stop',
+  StopLimit = 'StopLimit'
+}
+
+export enum BrokerageOrderDuration {
+  Day = 'Day',
+  Gtc = 'Gtc',
+  DayPlus = 'DayPlus',
+  GtcPlus = 'GtcPlus'
+}
