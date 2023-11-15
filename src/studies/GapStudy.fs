77ded7bb4c00c7f8f9552daba9d4e61c17ba570e
Laimonas Simutis <laimis@gmail.com>
2023-11-14 23:03:44 -0800

    separate data processing from running trade simulations

--- src/studies/GapStudy.fs
+++ src/studies/GapStudy.fs
@@ -7,7 +7,7 @@ open core.fs.Services.GapAnalysis
 open core.fs.Shared.Adapters.Stocks
 
 [<Literal>]
-let private minimumRecords = 10_000
+let private minimumRecords = 1_000
 [<Literal>]
 let private earliestYear = 2020
 [<Literal>]
@@ -36,13 +36,27 @@ let parseInput (inputFilename:string) =
             { date = date; ticker = ticker; screenerid = screenerid }
         )
         |> Seq.toArray
+        
+let parseOutput (filepath:string) =
+    
+    let csvFile = GapStudyOutput.Load(filepath)
+    
+    csvFile.Rows
+        |> Seq.map (fun row ->
+            let ticker = row.Ticker
+            let date = DateOnly.Parse row.Date
+            let screenerid = row.Screenerid
 
-let verifyInput records =
+            { date = date; ticker = ticker; screenerid = screenerid }
+        )
+        |> Seq.toArray
+
+let verifyRecords records =
     // make sure there is at least some records in here, ideally in thousands
     let numberOfRecords = records |> Array.length
     match numberOfRecords with
     | 0 -> failwith "no records"
-    | x when x < minimumRecords -> failwith "not enough records"
+    | x when x < minimumRecords -> failwith $"not enough records: {x}"
     | _ -> ()
     
     // make sure no data is blank
@@ -71,7 +85,7 @@ let verifyInput records =
     
     records
     
-let describeInput records =
+let describeRecords records =
     let numberOfRecords = records |> Array.length
     let dates = records |> Array.map (fun r -> r.date) |> Array.distinct |> Array.length
     let tickers = records |> Array.map (fun r -> r.ticker) |> Array.distinct |> Array.length
@@ -101,8 +115,8 @@ let study inputFilename (outputFilename:string) (priceFunc:DateTimeOffset -> Dat
     let records =
         inputFilename
         |> parseInput
-        |> verifyInput
-        |> describeInput
+        |> verifyRecords
+        |> describeRecords
 
     // generate a pair of ticker and the earliest data it is seen
     let tickerDatePairs = records |> getEarliestDateByTicker
@@ -173,5 +187,55 @@ let study inputFilename (outputFilename:string) (priceFunc:DateTimeOffset -> Dat
         
     let csvOutput = new GapStudyOutput(rows)
     csvOutput.Save outputFilename
-    
 }
+
+let runTrades matchedInputFilename (priceFunc:string -> PriceBars) =
+    
+    let data =
+         matchedInputFilename
+         |> parseOutput
+         |> verifyRecords
+         |> describeRecords
+    
+    let pl =
+        data
+        |> Array.map (fun r ->
+            let prices = r.ticker |> priceFunc
+            
+            let startBar = prices.Bars |> Array.find (fun b -> b.DateStr = r.date.ToString("yyyy-MM-dd"))
+            let endBar7 = prices.Bars |> Array.tryFind (fun b -> b.DateStr = r.date.AddDays(7).ToString("yyyy-MM-dd"))
+            let endBar30 = prices.Bars |> Array.tryFind (fun b -> b.DateStr = r.date.AddDays(30).ToString("yyyy-MM-dd"))
+            let endBar60 = prices.Bars |> Array.tryFind (fun b -> b.DateStr = r.date.AddDays(60).ToString("yyyy-MM-dd"))
+            let endBar90 = prices.Bars |> Array.tryFind (fun b -> b.DateStr = r.date.AddDays(90).ToString("yyyy-MM-dd"))
+            
+            let pl7 = match endBar7 with | Some endBar7 -> endBar7.Close - startBar.Close |> Some | None -> None
+            let pl30 = match endBar30 with | Some endBar30 -> endBar30.Close - startBar.Close |> Some | None -> None
+            let pl60 = match endBar60 with | Some endBar60 -> endBar60.Close - startBar.Close |> Some | None -> None
+            let pl90 = match endBar90 with | Some endBar90 -> endBar90.Close - startBar.Close |> Some | None -> None
+            
+            (r, pl7, pl30, pl60, pl90)
+        )
+        
+    // number of records with pl > 0
+    let pl7 = pl |> Array.choose (fun (r, pl7, _, _, _) -> match pl7 with | Some pl7 -> Some (r, pl7) | None -> None)  |> Array.partition (fun (_, pl7) -> pl7 > 0m)
+    let pl30 = pl |> Array.choose (fun (r, _, pl30, _, _) -> match pl30 with | Some pl30 -> Some (r, pl30) | None -> None)  |> Array.partition (fun (_, pl30) -> pl30 > 0m)
+    let pl60 = pl |> Array.choose (fun (r, _, _, pl60, _) -> match pl60 with | Some pl60 -> Some (r, pl60) | None -> None)  |> Array.partition (fun (_, pl60) -> pl60 > 0m)
+    let pl90 = pl |> Array.choose (fun (r, _, _, _, pl90) -> match pl90 with | Some pl90 -> Some (r, pl90) | None -> None)  |> Array.partition (fun (_, pl90) -> pl90 > 0m)
+    
+    let describeOutcome label plSection =
+        let wins = plSection |> fst
+        let losses = plSection |> snd
+        
+        let winGains = wins |> Array.map (fun (_, pl) -> pl) |> Array.sum
+        let lossGains = losses |> Array.map (fun (_, pl) -> pl) |> Array.sum
+        
+        printfn $"{label} positive: %d{wins |> Array.length}, %f{float (wins |> Array.length) / float pl.Length}"
+        printfn $"{label} win gains: %f{winGains}"
+        printfn $"{label} loss gains: %f{lossGains}"
+        printfn ""
+    
+    pl7 |> describeOutcome "PL7"
+    pl30 |> describeOutcome "PL30"
+    pl60 |> describeOutcome "PL60"
+    pl90 |> describeOutcome "PL90"
+    
--- src/studies/Program.fs
+++ src/studies/Program.fs
@@ -1,87 +1,15 @@
 ﻿// Gap study
 // read input from file 01_export_date_ticker_screenerid.csv
 open System
-open Microsoft.Extensions.Logging
-open core.fs.Shared.Adapters.Brokerage
-open core.fs.Shared.Adapters.Stocks
-open core.fs.Shared.Adapters.Storage
-open core.fs.Shared.Domain.Accounts
 open studies
 
 let studiesDirectory = "d:\studies"
 
-let getUser (storage:IAccountStorage) email = async {
-    return! storage.GetUserByEmail email |> Async.AwaitTask
-}
 
-type PriceAvailability =
-    | Available
-    | NotAvailableForever
-    | NotAvailable
-
-let getPrices (user:User) (brokerage:IBrokerage) startDate endDate ticker = async {
-    
-    // check first if we have prices for this ticker and date range
-    // if we do, return them
-    // if we don't, get them from brokerage and store them on a filesystem
-    // return them
-    let filename = $"{ticker}_{startDate:yyyyMMdd}_{endDate:yyyyMMdd}.csv"
-    let path = $"{studiesDirectory}\\{filename}"
-    
-    let recordError message =
-        ServiceHelper.logger.LogCritical("Error getting price history for {ticker}: {error}", ticker, message)
-        let csvContent = $"ERROR: {message}"
-        System.IO.File.WriteAllText(path, csvContent)
-    
-    let recordPrices (prices:PriceBars) =
-        // store prices on a filesystem, filename should contain ticker and date range
-        let csv = prices.Bars |> Array.map (fun p -> p.ToString()) |> String.concat Environment.NewLine
-        System.IO.File.WriteAllText(path, csv)
-        
-    let readPricesFromCsv() =
-        ServiceHelper.logger.LogInformation("Reading prices from file {path}", path)
-        let csv = System.IO.File.ReadAllLines(path)
-        let bars = csv |> Array.map PriceBar |> PriceBars
-        bars
-        
-    let pricesAvailableOnFileSystem() =
-        match System.IO.File.Exists(path) with
-        | false -> NotAvailable
-        | true ->
-            let content = System.IO.File.ReadAllLines(path)
-            match content with
-            | [||] -> NotAvailable
-            | _ ->
-                let firstLine = content[0]
-                match firstLine with
-                | x when x.Contains("No candles for historical prices for") -> NotAvailableForever
-                | x when x.Contains("Invalid open price") -> NotAvailableForever
-                | x when x.Contains("ERROR") -> NotAvailable
-                | _ -> Available
-        
-    match pricesAvailableOnFileSystem() with
-    | Available -> return readPricesFromCsv() |> Some
-    | NotAvailableForever -> return None
-    | NotAvailable ->
-        try
-            let! response = brokerage.GetPriceHistory user.State ticker PriceFrequency.Daily startDate endDate |> Async.AwaitTask
-            let result = response.Result
-            match result with
-            | Ok prices ->
-                recordPrices prices
-                return Some prices
-            | Error e ->
-                recordError e.Message
-                return None
-        with
-            | e ->
-                recordError e.Message
-                return None
-}
 
 Environment.GetCommandLineArgs() |> ServiceHelper.init
 
-let user = "laimis@gmail.com" |> getUser (ServiceHelper.storage()) |> Async.RunSynchronously
+let user = "laimis@gmail.com" |> DataHelpers.getUser (ServiceHelper.storage()) |> Async.RunSynchronously
 match user with
 | None -> failwith "User not found"
 | Some _ -> ()
@@ -89,6 +17,9 @@ match user with
 let inputFilename = $"{studiesDirectory}\\01_export_date_ticker_screenerid.csv"
 let outputFilename = $"{studiesDirectory}\\02_export_date_ticker_screenerid_gap.csv"
 
-let priceFunction = getPrices user.Value (ServiceHelper.brokerage())
+let getPricesWithBrokerage = DataHelpers.getPricesWithBrokerage user.Value (ServiceHelper.brokerage()) studiesDirectory
+let getPricesFromCsv = DataHelpers.getPricesFromCsv studiesDirectory
 
-GapStudy.study inputFilename outputFilename priceFunction |> Async.RunSynchronously
+// GapStudy.study inputFilename outputFilename priceFunction |> Async.RunSynchronously
+
+GapStudy.runTrades outputFilename getPricesFromCsv
