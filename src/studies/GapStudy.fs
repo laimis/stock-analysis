dec368877d558aee58f4ff818283edf57479ce98
Laimonas Simutis <laimis@gmail.com>
2023-11-17 11:00:25 -0800

    add jupyter notebook

--- src/studies/GapStudy.fs
+++ src/studies/GapStudy.fs
@@ -198,52 +198,6 @@ let study (inputFilename:string) (outputFilename:string) (priceFunc:DateTimeOffs
     let csvOutput = new GapStudyOutput(rows)
     csvOutput.Save outputFilename
 }
-
-module TradingStrategies =
-    let buyAndHoldStrategy (prices:PriceBars) numberOfBarsToHold (signal:GapStudyOutput.Row) =
-        // we will buy this stock at the open price of the next day
-        let name =
-            match numberOfBarsToHold with
-            | None -> "Buy and Hold"
-            | Some numberOfDaysToHold -> $"Buy and Hold {numberOfDaysToHold} bars"
-            
-        // find the next day
-        let openDay, openDayIndex =
-            match signal.Date.AddDays(1) |> prices.TryFindByDate with
-            | None -> signal.Date |> prices.TryFindByDate |> Option.get // if we can't find the next day, then just open at the signal date
-            | Some openDay -> openDay
-        
-        // find the close day
-        let closeBar =
-            match numberOfBarsToHold with
-            | None -> prices.Last
-            | Some numberOfDaysToHold ->
-                let closeDayIndex = openDayIndex + numberOfDaysToHold
-                match closeDayIndex with
-                | x when x >= prices.Length -> prices.Last
-                | _ -> prices.Bars[closeDayIndex]
-        
-        let openPrice = openDay.Open
-        let closePrice = closeBar.Close
-        
-        // calculate gain percentage
-        let gain = (closePrice - openPrice) / openPrice * 100m
-        
-        let daysHeld = closeBar.Date - openDay.Date
-        
-        TradeOutcomeOutput.Row(
-            strategy=name,
-            ticker=signal.Ticker,
-            date=signal.Date,
-            screenerid=signal.Screenerid,
-            hasGapUp=signal.HasGapUp,
-            opened=openDay.Date.DateTime,
-            openPrice=openPrice,
-            closed=closeBar.Date.DateTime,
-            closePrice=closePrice,
-            percentGain=gain,
-            numberOfDaysHeld=(daysHeld.TotalDays |> int)
-        )
     
 let runStrategy strategy dataWithPriceBars =
     dataWithPriceBars
@@ -261,7 +215,7 @@ let runTrades (matchedInputFilename:string) (priceFunc:string -> Async<PriceBars
     
     data |> Seq.map Output |> describeRecords
     
-    // ridiculous, sometimes input data does not have prices for the date
+    // ridiculous, sometimes data provider does not have prices for the date
     // so we filter those records out
     let! asyncData =
         data
