cc878d9eb51de2c7f91fe92736b406ab182fea71
Laimonas Simutis <laimis@gmail.com>
2023-12-07 11:12:33 -0800

    add loading and error indicators to position outcomes report

--- src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-outcomes-reports.component.ts
+++ src/web/ClientApp/src/app/stocks/stock-trading/stock-trading-outcomes-reports.component.ts
@@ -19,12 +19,29 @@ export class StockPositionReportsComponent {
   gaps: StockGaps[] = [];
   tickerFilter: string;
 
+  loading = {
+    daily: false,
+    weekly: false,
+    allBars: false,
+    positions: false
+  }
+
+  errors = {
+    daily: null,
+    weekly: null,
+    allBars: null,
+    positions: null
+  }
+
 
 	constructor(private service : StocksService){}
 
   @Input()
   set dailyAnalysis(value:PositionInstance[]) {
     if (value) {
+      this.loading.daily = true
+      this.loading.weekly = true
+      this.loading.positions = true
       this.loadPositionData(value)
     }
   }
@@ -32,21 +49,23 @@ export class StockPositionReportsComponent {
   @Input()
   set allTimeAnalysis(value:PositionInstance[]) {
     if (value) {
+      this.loading.allBars = true
       this.loadAllTimeData(value)
     }
   }
 
   tickers: string[] = []
-  errors: string[] = null
   loadDailyData(positions:PositionInstance[]) {
     this.tickers = positions.map(p => p.ticker)
     this.service.reportOutcomesSingleBarDaily(this.tickers).subscribe(
       report => {
+        this.loading.daily = false
         this.singleBarReportDaily = report
         this.loadWeeklyData(positions)
       },
       error => {
-        this.handleApiError("Unable to load daily data", error)
+        this.loading.daily = false
+        this.handleApiError("Unable to load daily data", error, (e) => this.errors.daily = e)
         this.loadWeeklyData(positions)
       }
     )
@@ -55,36 +74,43 @@ export class StockPositionReportsComponent {
   loadWeeklyData(positions:PositionInstance[]) {
     this.tickers = positions.map(p => p.ticker)
     this.service.reportOutcomesSingleBarWeekly(this.tickers).subscribe(report => {
+      this.loading.weekly = false
       this.singleBarReportWeekly = report
     }, error => {
-      this.handleApiError("Unable to load weekly data", error)
+      this.loading.weekly = false
+      this.handleApiError("Unable to load weekly data", error, (e) => this.errors.weekly = e)
     })
 
   }
 
   loadPositionData(positions:PositionInstance[]) {
     this.service.reportPositions().subscribe(report => {
+      this.loading.positions = false
       this.positionsReport = report
       this.loadDailyData(positions)
     }, error => {
-      this.handleApiError("Unable to load position reports", error)
+      this.loading.positions = false
+      this.handleApiError("Unable to load position reports", error, (e) => this.errors.positions = e)
       this.loadDailyData(positions)
     })
   }
 
-  private handleApiError(errorMessage: string, error: any) {
-    var forConsole = GetErrors(error)
-    forConsole.forEach(e => console.log(e))
-    this.errors = [errorMessage]
+  private handleApiError(errorMessage: string, error: any, assignFunc : (error:any) => void) {
+    const extractedErrors = GetErrors(error);
+    extractedErrors.forEach(e => console.log(e))
+    const fullError = errorMessage + ": " + extractedErrors.join(", ")
+    assignFunc([fullError])
   }
 
   loadAllTimeData(positions:PositionInstance[]) {
     this.tickers = positions.map(p => p.ticker)
     this.service.reportOutcomesAllBars(this.tickers).subscribe(report => {
+      this.loading.allBars = false
       this.allBarsReport = report
       this.gaps = report.gaps
     }, error => {
-      this.handleApiError("Unable to load all bars", error)
+      this.loading.allBars = false
+      this.handleApiError("Unable to load all bars", error, (e) => this.errors.allBars = e)
     })
   }
 
