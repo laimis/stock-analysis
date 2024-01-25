1eb99d4d189c2f9a96d9882bac3d99311a253d8d
Laimonas Simutis <laimis@gmail.com>
2024-01-25 09:10:26 -0800

    add more tests and more studies scenarios

--- src/studies/Types.fs
+++ src/studies/Types.fs
@@ -7,10 +7,10 @@ open core.fs.Adapters.Stocks
 module Constants =
     [<Literal>]
     let MinimumRecords = 1_000
-    
+
     [<Literal>]
     let EarliestYear = 2020
-    
+
     [<Literal>]
     let LatestYear = 2024
 
@@ -25,13 +25,13 @@ type SignalWithPriceProperties =
         Schema = "screenerid (int), date (string), ticker (string), gap (decimal), sma20 (decimal), sma50 (decimal), sma150 (decimal), sma200 (decimal)",
         HasHeaders=false
     >
-    
+
 type TradeOutcomeOutput =
     CsvProvider<
         Sample = "screenerid (int), date (string), ticker (string), gap (decimal), sma20 (decimal), sma50 (decimal), sma150 (decimal), sma200 (decimal), strategy (string), longOrShort (string), opened (string), openPrice (decimal), closed (string), closePrice (decimal), percentGain (decimal), numberOfDaysHeld (int)",
         HasHeaders=true
     >
-    
+
 module Signal =
     let verifyRecords (input:Signal) minimumRecordsExpected =
         let records = input.Rows
@@ -40,23 +40,23 @@ module Signal =
         match numberOfRecords with
         | x when x < minimumRecordsExpected -> failwith $"{x} is not enough records, expecting at least {minimumRecordsExpected}"
         | _ -> ()
-        
+
         let verifyCondition failureConditionFunc messageIfFound =
             records
             |> Seq.map (fun r -> match failureConditionFunc r with | true -> Some r | false -> None)
             |> Seq.choose id
             |> Seq.tryHead
             |> Option.iter (fun r -> r |> messageIfFound |> failwith)
-        
+
         // make sure all the dates are set (and can be parsed?)
         let invalidDate = fun (r:Signal.Row) -> match DateTimeOffset.TryParse(r.Date) with | true, _ -> false | false, _ -> true
         let messageIfInvalidDate = fun (r:Signal.Row) -> $"date is invalid for record {r.Screenerid}, {r.Ticker}"
         verifyCondition invalidDate messageIfInvalidDate
-        
+
         let invalidTicker = fun (r:Signal.Row) -> String.IsNullOrWhiteSpace(r.Ticker)
         let messageIfInvalidTicker = fun (r:Signal.Row) -> $"ticker is blank for record {r.Screenerid}, {r.Date}"
         verifyCondition invalidTicker messageIfInvalidTicker
-        
+
         let invalidScreenerId = fun (r:Signal.Row) -> r.Screenerid = 0
         let messageIfInvalidScreenerId = fun (r:Signal.Row) -> $"screenerid is blank for record {r.Ticker}, {r.Date}"
         verifyCondition invalidScreenerId messageIfInvalidScreenerId
@@ -67,59 +67,59 @@ type Unified =
     | Input of Signal.Row
     | Output of SignalWithPriceProperties.Row
 
-module Unified =    
+module Unified =
     let describeRecords (records:Unified seq) =
-        
+
         let getDate unified =
             match unified with
             | Input row -> row.Date
             | Output row -> row.Date
-            
+
         let getTicker unified =
             match unified with
             | Input row -> row.Ticker
             | Output row -> row.Ticker
-            
+
         let getScreenerId unified =
             match unified with
             | Input row -> row.Screenerid
             | Output row -> row.Screenerid
-        
+
         let numberOfRecords = records |> Seq.length
-        
+
         match numberOfRecords with
         | x when x > 0 ->
             let dates = records |> Seq.map (fun r -> r |> getDate) |> Seq.distinct |> Seq.length
             let tickers = records |> Seq.map (fun r -> r |> getTicker) |> Seq.distinct |> Seq.length
             let screenerIds = records |> Seq.map (fun r -> r |> getScreenerId) |> Seq.distinct |> Seq.length
-            
+
             let minimumDate = records |> Seq.minBy (fun r -> r |> getDate) |> getDate
             let maximumDate = records |> Seq.maxBy (fun r -> r |> getDate) |> getDate
-            
+
             printfn $"Records: %d{numberOfRecords}, dates: %d{dates}, tickers: %d{tickers}, screenerIds: %d{screenerIds}"
             printfn $"Minimum date: %A{minimumDate}"
             printfn $"Maximum date: %A{maximumDate}"
             printfn ""
         | _ ->
             printfn $"No records found in the input"
-    
+
 module TradeOutcomeOutput =
-    
+
     let create strategy positionType (signal:SignalWithPriceProperties.Row) (openBar:PriceBar) (closeBar:PriceBar) =
-        
+
         let openPrice = openBar.Open
         let closePrice = closeBar.Close
-        
+
         let daysHeld = closeBar.Date - openBar.Date
-        
+
         let longOrShortString, gainFactor =
             match positionType with
             | core.fs.Stocks.StockPositionType.Long -> "long", 1m
             | core.fs.Stocks.StockPositionType.Short -> "short", -1m
-        
+
         // calculate gain percentage
         let gain = (closePrice - openPrice) / openPrice * gainFactor
-        
+
         TradeOutcomeOutput.Row(
             screenerid=signal.Screenerid,
             date=signal.Date,
@@ -163,16 +163,25 @@ module TradeSummary =
         let numberOfWinners = winners |> Seq.length
         let numberOfLosers = losers |> Seq.length
         let win_pct = decimal numberOfWinners / decimal numberOfTrades
-        let avg_win = winners |> Seq.averageBy (_.PercentGain)
-        let avg_loss = losers |> Seq.averageBy (_.PercentGain)
-        let avg_gain_loss = avg_win / avg_loss |> Math.Abs
+        let avg_win =
+            match numberOfWinners with
+            | 0 -> 0m
+            | _ -> winners |> Seq.averageBy (_.PercentGain)
+        let avg_loss =
+            match numberOfLosers with
+            | 0 -> 0m
+            | _ -> losers |> Seq.averageBy (_.PercentGain)
+        let avg_gain_loss =
+            match avg_loss with
+            | 0m -> 0m
+            | _ -> avg_win / avg_loss |> Math.Abs
         let ev = win_pct * avg_win - (1m - win_pct) * (avg_loss |> Math.Abs)
         let totalGain = outcomes |> Seq.sumBy (_.PercentGain)
 
         let gains = outcomes |> Seq.map (_.PercentGain)
         let gainDistribution = core.fs.Services.Analysis.DistributionStatistics.calculate gains
-        
-        // return trade summary 
+
+        // return trade summary
         {
             StrategyName = name
             NumberOfTrades = numberOfTrades
@@ -185,5 +194,5 @@ module TradeSummary =
             AvgGainLoss = avg_gain_loss
             EV = ev
             Gains = gains
-            GainDistribution = gainDistribution 
+            GainDistribution = gainDistribution
         }
--- tests/studiestests/TypeTests.fs
+++ tests/studiestests/TypeTests.fs
@@ -10,59 +10,55 @@ let round (number:decimal) = Math.Round(number, 4)
 
 [<Fact>]
 let ``loading signals csv works`` () =
-    
+
     let signals = studies.Types.Signal.Load(StudiesTestData.SignalsPath);
-    
+
     signals.Rows |> Seq.length |> should equal 48
-    
+
 [<Fact>]
 let ``loading transformed csv works`` () =
     let signals = studies.Types.Signal.Load(StudiesTestData.SignalsTransformedPath);
     signals.Rows |> Seq.length |> should equal 32
-    
+
 [<Fact>]
 let ``Verifying test data should pass`` () =
     let signals = studies.Types.Signal.Load(StudiesTestData.SignalsTransformedPath);
     Signal.verifyRecords signals 10 |> ignore
-    
+
 [<Fact>]
 let ``verifyRecords throws exception when there are no records``() =
     let emptySignal = new Signal()
     (fun () -> Signal.verifyRecords emptySignal 100 |> ignore)
     |> should (throwWithMessage "0 is not enough records, expecting at least 100") typeof<Exception>
-    
+
 [<Fact>]
 let ``Verification fails if ticker is blank``() =
     let row = studies.Types.Signal.Row(date="2023-01-01", ticker="", screenerid=1)
     let signals = new Signal([row])
     (fun () -> Signal.verifyRecords signals 1 |> ignore)
     |> should (throwWithMessage "ticker is blank for record 1, 2023-01-01") typeof<Exception>
-    
+
 [<Fact>]
 let ``Verification fails if date is blank``() =
     let row = studies.Types.Signal.Row(date="", ticker=TestDataGenerator.NET.Value, screenerid=1)
     let signals = new Signal([row])
     (fun () -> Signal.verifyRecords signals 1 |> ignore)
     |> should (throwWithMessage "date is invalid for record 1, NET") typeof<Exception>
-    
+
 [<Fact>]
 let ``Verification fails if screenerid is blank``() =
     let row = studies.Types.Signal.Row(date="2023-01-01", ticker=TestDataGenerator.NET.Value, screenerid=0)
     let signals = new Signal([row])
     (fun () -> Signal.verifyRecords signals 1 |> ignore)
     |> should (throwWithMessage "screenerid is blank for record NET, 2023-01-01") typeof<Exception>
-    
+
 [<Fact>]
 let ``Describe records for Signal works``() =
     let signals = studies.Types.Signal.Load(StudiesTestData.SignalsPath)
     signals.Rows |> Seq.map Input |> Unified.describeRecords
-    
-[<Fact>]
-let ``Manual trade outcomes``() =
 
-    let gains = [0.02m; 0.01m; 1m; -0.05m; -0.08m]
-    let outcomes =
-        gains
+let generateTradeOutcomes gains =
+    gains
         |> List.map( fun g ->
             TradeOutcomeOutput.Row(
                     screenerid = 1, date = "2022-08-08", ticker = "NET",
@@ -74,9 +70,14 @@ let ``Manual trade outcomes``() =
                     numberOfDaysHeld = 9
                 )
         )
-    
+
+[<Fact>]
+let ``Creating trade outcomes works``() =
+
+    let outcomes = [0.02m; 0.01m; 1m; -0.05m; -0.08m] |> generateTradeOutcomes
+
     let summary = TradeSummary.create "B&H with trailing stop" outcomes
-    
+
     summary.Gains |> Seq.map (fun g -> Math.Round(g, 4)) |> should equal [0.02m; 0.01m; 1m; -0.05m; -0.08m]
     summary.Losers |> should equal 2
     summary.NumberOfTrades |> should equal 5
@@ -87,4 +88,11 @@ let ``Manual trade outcomes``() =
     summary.WinPct |> round |> should equal 0.6m
     summary.AvgGainLoss |> round |> should equal 5.2821m
     summary.StrategyName |> should equal "B&H with trailing stop"
-    
+
+[<Fact>]
+let ``Trade outcomes with zero wins works`` () =
+    [-0.05m] |> generateTradeOutcomes |> TradeSummary.create "B&H with trailing stop" |> ignore
+
+[<Fact>]
+let ``Trade outcomes with zero losses works`` () =
+    [0.05m] |> generateTradeOutcomes |> TradeSummary.create "B&H with trailing stop" |> ignore
