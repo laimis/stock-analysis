be69df84e4914505280ab18b01a59cad48268b3e
Laimonas Simutis <laimis@gmail.com>
2024-03-13 11:30:45 -0700

    flexible container rendering

--- src/web/ClientApp/src/app/reports/trends-report/trends-report.component.ts
+++ src/web/ClientApp/src/app/reports/trends-report/trends-report.component.ts
@@ -40,19 +40,8 @@ export class TrendsReportComponent implements OnInit {
     selectedStartDate: string;
     selectedTrendType: TrendType;
     
-    upBarDistribution: DataPointContainer
-    downBarDistribution: DataPointContainer
-    upGainDistribution: DataPointContainer
-    downGainDistribution: DataPointContainer
+    dataPointContainers: DataPointContainer[] = []
     
-    upBarSorted: DataPointContainer
-    downBarSorted: DataPointContainer
-    upGainSorted: DataPointContainer
-    downGainSorted: DataPointContainer
-    
-    barsChronological: DataPointContainer
-    gainsChronological: DataPointContainer
-
     constructor(private stockService:StocksService) {
         // Set default values
         this.selectedTicker = "SPY";
@@ -73,25 +62,28 @@ export class TrendsReportComponent implements OnInit {
         this.stockService.reportTrends(this.selectedTicker, this.selectedTrendType, selectedStartDate).subscribe(data => {
             this.trends = data
             this.currentTrend = data.currentTrend
-            this.upBarDistribution = this.createContainer(data.upBarStatistics.buckets, "Up Bar Distribution")
-            this.downBarDistribution = this.createContainer(data.downBarStatistics.buckets, "Down Bar Distribution")
-            this.upGainDistribution = this.createContainer(data.upGainStatistics.buckets, "Up Gain Distribution")
-            this.downGainDistribution = this.createContainer(data.downGainStatistics.buckets, "Down Gain Distribution")
-            
-            // up bars sorted
-            this.upBarSorted = this.sortAndCreateContainer(data.upTrends.map((t:Trend) => t.numberOfBars), "Up Bars Sorted")
-            this.downBarSorted = this.sortAndCreateContainer(data.downTrends.map((t:Trend) => t.numberOfBars), "Down Bars Sorted")
-            this.upGainSorted = this.sortAndCreateContainer(data.upTrends.map((t:Trend) => t.gainPercent), "Up Gain Sorted")
-            this.downGainSorted = this.sortAndCreateContainer(data.downTrends.map((t:Trend) => t.gainPercent), "Down Gain Sorted")
-            
+
             // chronological
             let barsChronological: DataPoint[] = data.trends.map((t:Trend) => {
                 return {label:t.startDateStr, isDate: true, value: t.direction === TrendDirection.Up ? t.numberOfBars : -t.numberOfBars}
             })
-            this.barsChronological = {label: "Bars Chronological", chartType: ChartType.Column, data: barsChronological}
-            
             let gainsChronological: DataPoint[] = data.trends.map((t:Trend) => {return {label:t.startDateStr, isDate: true, value: t.gainPercent}})
-            this.gainsChronological = {label: "Gains Chronological", chartType: ChartType.Column, data: gainsChronological}
+
+            this.dataPointContainers = [
+                this.createContainer(data.upBarStatistics.buckets, "Up Bar Distribution"),
+                this.createContainer(data.downBarStatistics.buckets, "Down Bar Distribution"),
+                this.createContainer(data.upGainStatistics.buckets, "Up Gain Distribution"),
+                this.createContainer(data.downGainStatistics.buckets, "Down Gain Distribution"),
+                
+                // up bars sorted
+                this.sortAndCreateContainer(data.upTrends.map((t:Trend) => t.numberOfBars), "Up Bars Sorted"),
+                this.sortAndCreateContainer(data.downTrends.map((t:Trend) => t.numberOfBars), "Down Bars Sorted"),
+                this.sortAndCreateContainer(data.upTrends.map((t:Trend) => t.gainPercent), "Up Gain Sorted"),
+                this.sortAndCreateContainer(data.downTrends.map((t:Trend) => t.gainPercent), "Down Gain Sorted"),
+                
+                {label: "Bars Chronological", chartType: ChartType.Column, data: barsChronological},
+                {label: "Gains Chronological", chartType: ChartType.Column, data: gainsChronological}
+            ]
         });
     }
     
@@ -111,6 +103,7 @@ export class TrendsReportComponent implements OnInit {
     applyFilters() {
         this.currentTrend = null
         this.trends = null
+        this.dataPointContainers = null
         this.loadTrends()
     }
     
