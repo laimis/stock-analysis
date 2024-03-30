3b56e42cd71f7a9ab606f0be3b4f2072c765d641
Laimonas Simutis <laimis@gmail.com>
2024-03-30 12:39:57 -0700

    remove unused code

--- test_database.bat
+++ test_database.bat
@@ -1,5 +1,1 @@
 dotnet test --configuration Release --filter="Category=Postgres" 
-    @REM /p:CollectCoverage=true^
-    @REM /p:CoverletOutputFormat=opencover^
-    
-@REM reportgenerator -reports:tests/coretests/coverage.opencover.xml;tests/storagetests/coverage.opencover.xml -targetdir:coverage
