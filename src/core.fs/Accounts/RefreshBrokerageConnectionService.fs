e313f54a31fb9d90dd1402c42ff49f5b57a8a642
Laimonas Simutis <laimis@gmail.com>
2024-08-26 14:58:11 -0700

    change refresh token to use email notice

--- src/core.fs/Accounts/RefreshBrokerageConnectionService.fs
+++ src/core.fs/Accounts/RefreshBrokerageConnectionService.fs
@@ -4,10 +4,14 @@ open System
 open System.Threading
 open core.fs
 open core.fs.Adapters.Brokerage
+open core.fs.Adapters.Email
 open core.fs.Adapters.Logging
 open core.fs.Adapters.Storage
-    
-type RefreshBrokerageConnectionService(accounts:IAccountStorage,brokerage:IBrokerage,logger:ILogger) =
+
+// running this on a sechedule to refresh brokerage connections
+// the issue with Schwab is that they no longer allowing to renew
+// so I changed this job to email the user instead and tell them to go ahead and refresh the token    
+type RefreshBrokerageConnectionService(accounts:IAccountStorage,brokerage:IBrokerage,logger:ILogger,email:IEmailService) =
     
     let runBrokerageCheck userId = task {
         
@@ -23,17 +27,24 @@ type RefreshBrokerageConnectionService(accounts:IAccountStorage,brokerage:IBroke
             | true ->
                 match user.State.BrokerageRefreshTokenExpires.Date <= expirationThreshold with
                 | true ->
-                    logger.LogInformation $"Brokerage connection for user {userId} is about to expire. Refreshing..."
+                    logger.LogInformation $"Brokerage connection for user {userId} is about to expire. Sending notice..."
                     
-                    let! refreshedToken = brokerage.RefreshAccessToken user.State
+                    let refreshUrl = "https://app.nightingaletrading.com/profile/"
                     
-                    match refreshedToken.IsError with
-                    | true ->
-                        logger.LogError $"Brokerage connection for user {userId} could not be refreshed. Error: {refreshedToken.error}"
-                    | false ->
-                        user.RefreshBrokerageConnection refreshedToken.access_token refreshedToken.refresh_token refreshedToken.token_type refreshedToken.expires_in refreshedToken.scope
-                        do! accounts.Save user
-                        logger.LogInformation $"Brokerage connection for user {userId} was refreshed."
+                    let message = $"Your brokerage connection is about to expire. Please refresh your connection by going to <a href=\"{refreshUrl}\">this link</a>."
+                    
+                    let emailInput = {EmailInput.Body = message; From = Sender.NoReply.Email; Subject = "Refresh Brokerage Account"; To = user.State.Email; FromName = Sender.NoReply.Name }
+                    
+                    do! email.SendWithInput emailInput
+                    // let! refreshedToken = brokerage.RefreshAccessToken user.State
+                    //
+                    // match refreshedToken.IsError with
+                    // | true ->
+                    //     logger.LogError $"Brokerage connection for user {userId} could not be refreshed. Error: {refreshedToken.error}"
+                    // | false ->
+                    //     user.RefreshBrokerageConnection refreshedToken.access_token refreshedToken.refresh_token refreshedToken.token_type refreshedToken.expires_in refreshedToken.scope
+                    //     do! accounts.Save user
+                    //     logger.LogInformation $"Brokerage connection for user {userId} was refreshed."
                 | false ->
                     logger.LogInformation $"Brokerage connection for user {userId} is valid until {user.State.BrokerageRefreshTokenExpires}. Skipping refresh."  
     }
