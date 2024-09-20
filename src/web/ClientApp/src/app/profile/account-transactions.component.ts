9efe5aed395477fb6a692cc0c182375304d899d1
Laimonas Simutis <laimis@gmail.com>
2024-09-20 09:55:54 -0700

    add sorting

--- src/web/ClientApp/src/app/profile/account-transactions.component.ts
+++ src/web/ClientApp/src/app/profile/account-transactions.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AccountTransaction, StocksService} from "../services/stocks.service";
 import {ParsedDatePipe} from "../services/parsedDate.filter";
-import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {CurrencyPipe, NgClass, NgForOf, NgIf} from "@angular/common";
 import {GetErrors} from "../services/utils";
 import {ErrorDisplayComponent} from "../shared/error-display/error-display.component";
 
@@ -13,17 +13,20 @@ import {ErrorDisplayComponent} from "../shared/error-display/error-display.compo
         CurrencyPipe,
         NgForOf,
         NgIf,
-        ErrorDisplayComponent
+        ErrorDisplayComponent,
+        NgClass
     ],
   templateUrl: './account-transactions.component.html',
   styleUrl: './account-transactions.component.css'
 })
 export class AccountTransactionsComponent implements OnInit {
     transactions: AccountTransaction[] = [];
-
-    constructor(private stocksService: StocksService) {}
+    sortColumn: keyof AccountTransaction = 'tradeDate';
+    sortDirection: 'asc' | 'desc' = 'desc';
     errors = [];
 
+    constructor(private stocksService: StocksService) {}
+    
     ngOnInit(): void {
         this.loadTransactions();
     }
@@ -32,6 +35,7 @@ export class AccountTransactionsComponent implements OnInit {
         this.stocksService.getAccountTransactions().subscribe(
             r => {
                 this.transactions = r;
+                this.sortTransactions();
             },
             error => {
                 this.errors = GetErrors(error);
@@ -48,5 +52,26 @@ export class AccountTransactionsComponent implements OnInit {
                 console.error('Error marking transaction as applied:', error);
             }
         );
+    }
+
+    sortTransactions(): void {
+        this.transactions.sort((a, b) => {
+            const aValue = a[this.sortColumn];
+            const bValue = b[this.sortColumn];
+
+            if (aValue < bValue) return this.sortDirection === 'asc' ? -1 : 1;
+            if (aValue > bValue) return this.sortDirection === 'asc' ? 1 : -1;
+            return 0;
+        });
+    }
+
+    changeSort(column: keyof AccountTransaction): void {
+        if (this.sortColumn === column) {
+            this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            this.sortColumn = column;
+            this.sortDirection = 'asc';
+        }
+        this.sortTransactions();
     }
 }
