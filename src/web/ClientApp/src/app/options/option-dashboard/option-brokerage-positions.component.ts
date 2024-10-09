9fd4eacecbe89c0690b2f619be67cf15d124ff19
Laimonas Simutis <laimis@gmail.com>
2024-10-09 09:39:15 -0700

    group options

--- src/web/ClientApp/src/app/options/option-dashboard/option-brokerage-positions.component.ts
+++ src/web/ClientApp/src/app/options/option-dashboard/option-brokerage-positions.component.ts
@@ -2,23 +2,46 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {BrokerageOptionPosition, StocksService} from 'src/app/services/stocks.service';
 import {GetErrors} from 'src/app/services/utils';
 
+
+export interface BrokerageOptionPositionCollection {
+    positions: BrokerageOptionPosition[]
+    cost: number
+    marketValue: number
+}
+
 @Component({
     selector: 'app-option-brokerage-positions',
     templateUrl: './option-brokerage-positions.component.html',
     styleUrls: ['./option-brokerage-positions.component.css']
 })
-
 export class OptionBrokeragePositionsComponent {
     errors: string[];
     private _positions: BrokerageOptionPosition[];
     totalMarketValue: number;
     totalCost: number;
+    positionCollections: BrokerageOptionPositionCollection[]
     @Input()
     set positions(val: BrokerageOptionPosition[])
     {
         this._positions = val
         this.totalMarketValue = this._positions.reduce((acc, pos) => acc + pos.marketValue, 0)
         this.totalCost = this._positions.reduce((acc, pos) => acc + pos.averageCost * pos.quantity, 0) * 100
+        
+        this.positionCollections = []
+        
+        let grouped = this._positions.reduce((acc, pos) => {
+            let key = pos.ticker
+            if (!acc[key]) acc[key] = []
+            acc[key].push(pos)
+            return acc
+        }, {} as {[key: string]: BrokerageOptionPosition[]})
+        
+        for (let key in grouped) {
+            let positions = grouped[key]
+            let cost = positions.reduce((acc, pos) => acc + pos.averageCost * pos.quantity, 0) * 100
+            let marketValue = positions.reduce((acc, pos) => acc + pos.marketValue, 0)
+            this.positionCollections.push({positions, cost, marketValue})
+        }
     }
     get positions() {
         return this._positions
