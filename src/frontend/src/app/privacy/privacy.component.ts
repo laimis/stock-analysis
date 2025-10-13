f145793ff148ca3f01e2b03a229bfe96483da9ae
Laimonas Simutis <laimis@gmail.com>
2025-10-13 11:14:47 -0700

    insane migration

--- src/frontend/src/app/privacy/privacy.component.ts
+++ src/frontend/src/app/privacy/privacy.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core';
     selector: 'app-privacy',
     templateUrl: './privacy.component.html',
     styleUrls: ['./privacy.component.css'],
-    standalone: false
+    standalone: true
 })
 export class PrivacyComponent {
 
